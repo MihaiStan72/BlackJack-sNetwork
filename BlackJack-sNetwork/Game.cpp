5c167dc567cb8cb17bb05c913123c41e4ebb85c1
AndreiTimofte96 <andrei.timofte96@gmail.com>
2017-01-13 01:16:21 +0200

    random cards not working

--- BlackJack-sNetwork/Game.cpp
+++ BlackJack-sNetwork/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include "UI.h" 
 #include "ConcreteDrawableObjects.h"
+#include <sstream>
 
 using namespace UI;
 using namespace ConcreteDrawableObjects;
@@ -8,6 +9,7 @@ using namespace ConcreteDrawableObjects;
 
 Game::Game::Game() {
 	gameState = State::Playing;
+	x_Player = 620; y_Player = 370;
 }
 
 void Game::Game::Start() {
@@ -25,9 +27,7 @@ void Game::Game::readCards(int &cardsNumber) {
 	}
 }
 
-void  Game::Game::getRandomPath(char path[MAX_CARDS], int &number) {
-
-	readCards(cardsNumber);
+void Game::Game::getRandomPath(char path[MAX_CARDS], int &number){
 
 	srand(time(NULL));
 	int randomNumber;
@@ -38,15 +38,19 @@ void  Game::Game::getRandomPath(char path[MAX_CARDS], int &number) {
 	for (index = 0; cards.Matrix[randomNumber][index]; index++) {
 		path[index] = cards.Matrix[randomNumber][index];
 	}
+	std::cout << number<< '\n';
 	path[index] = '\0';
 }
 
 void Game::Game::create_playerCardsVector() {
-
+	
 	for (int index = 0; index < MAX_PLAYER_CARDS; index++){
 		playerCards.shownCards[index] = new Imagine();
-		playerCards.shownCards[index]->setPosition(UI::Position(100, 100));///////*****************
+		playerCards.shownCards[index]->setPosition(UI::Position(x_Player, y_Player));
+		x_Player += 40; y_Player += 20;
 		getRandomPath(path, number);
+		std::cout << path << '\n';
+		std::cout << number <<  '\n';
 		playerCards.shownCards[index]->setPath(path);
 		playerCards.values[index] = number;
 	}
@@ -54,8 +58,8 @@ void Game::Game::create_playerCardsVector() {
 
 void Game::Game::draw_playerTwoCards() {
 
-	DrawAgent::GetReference().Add(playerCards.shownCards[0]);
-	DrawAgent::GetReference().Add(playerCards.shownCards[1]);
+	DrawAgent::GetReference().Add(playerCards.shownCards[shownIndex++]);
+	DrawAgent::GetReference().Add(playerCards.shownCards[shownIndex++]);
 }
 
 void Game::Game::Loop() {
@@ -65,9 +69,11 @@ void Game::Game::Loop() {
 	DrawAgent::GetReference().Add(table);
 
 	//place_playerBets();
+	readCards(cardsNumber);
 	create_playerCardsVector();
 	draw_playerTwoCards();
 	//create_dealerCardsVector();
+
 
 	while (gameState != State::Exiting){
 		
@@ -76,6 +82,12 @@ void Game::Game::Loop() {
 			gameState = State::Exiting;
 			break;
 		}
+
+		//if (sumPlayer < BJ_CONDITION && event == WindowManager::WindowEvent::LeftClick) {
+
+			//DrawAgent::GetReference().Add(playerCards.shownCards[shownIndex++]);
+		//}
+
 		DrawAgent::GetReference().UpdateFrame();
 	}
 }
--- BlackJack-sNetwork/Game.h
+++ BlackJack-sNetwork/Game.h
@@ -1,10 +1,11 @@
 #include "Networking.h"
 #include "UI.h" 
 #include <fstream>
+#include <iostream>
 #include "ConcreteDrawableObjects.h"
-#include <sstream>
 #define MAX_CARDS 60
 #define MAX_PLAYER_CARDS 11
+#define BJ_CONDITION 21
 
 using namespace UI;
 using namespace ConcreteDrawableObjects;
@@ -34,11 +35,12 @@ namespace Game {
 		}cards;
 
 		struct{
-			Imagine *shownCards[MAX_PLAYER_CARDS]	;
+			Imagine *shownCards[MAX_PLAYER_CARDS];
 			int values[MAX_PLAYER_CARDS];
 		}playerCards, dealerCards;
 		char path[MAX_CARDS];
-		int number=0, cardsNumber=0;
+		int cardsNumber=0, sumPlayer=0, sumDealer=0, shownIndex=0, number=0;
+		float x_Player, y_Player, x_Dealer, y_Dealer;
 	};
 
 	class Player {
